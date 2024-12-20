d6d2abe18b5708935e3de50d643bc217225dc7e2
sana <sana@DESKTOP-2VN85IF.mshome.net>
2024-12-20 18:12:07 -0500

    1st push

---
+++ mavenpractiseproject/src/test/java/pac1/ContactTest.java
@@ -0,0 +1,24 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest
+{
+	@Test
+	public void createContact() {
+		System.out.println("create contact");
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
+	}
+	@Test
+	public void modifycontact() {
+		System.out.println("modify contact");
+	}
+}
---
+++ mavenpractiseproject/src/test/java/pac2/OrgTest.java
@@ -0,0 +1,15 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest
+{
+	@Test
+	public void createOrg() {
+		System.out.println("create org");
+	}
+	@Test
+	public void modifyorg() {
+		System.out.println("modify org");
+	}
+}
